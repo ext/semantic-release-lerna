f6b2b2e4175f97105e1c3ff82b88abce9c59b775
David Sveningsson <ext@sidvind.com>
2024-03-21 02:23:49 +0100

    fix(deps): remove read-pkg dependency

--- src/get-pkg.js
+++ src/get-pkg.js
@@ -1,11 +1,14 @@
 import path from "node:path";
-import { readPackage } from "read-pkg";
+import fs from "node:fs/promises";
 import AggregateError from "aggregate-error";
 import getError from "./get-error.js";
 
 export default async function ({ pkgRoot }, { cwd }) {
 	try {
-		const pkg = await readPackage({ cwd: pkgRoot ? path.resolve(cwd, String(pkgRoot)) : cwd });
+		const rootDir = pkgRoot ? path.resolve(cwd, String(pkgRoot)) : cwd;
+		const filePath = path.join(rootDir, "package.json");
+		const content = await fs.readFile(filePath, "utf-8");
+		const pkg = JSON.parse(content);
 
 		if (!pkg.name) {
 			throw getError("ENOPKGNAME");
