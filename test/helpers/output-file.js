c52fd78faab9a5d5e75dd1f79f5a7edd44ea23c0
Sample User <you@example.com>
2024-06-22 19:51:21 +0200

    refactor: drop dependency fs-extra

---
+++ test/helpers/output-file.js
@@ -0,0 +1,11 @@
+import fs from "node:fs/promises";
+import { existsSync } from "node:fs";
+import path from "node:path";
+
+export async function outputFile(file, data) {
+	const dir = path.dirname(file);
+	if (!existsSync(dir)) {
+		await fs.mkdir(dir, { recursive: true });
+	}
+	await fs.writeFile(file, data, "utf-8");
+}
